42905a7280e1aaaf563c53c466d2e2ece957f438
Jan Calimlim <105407268+januscali@users.noreply.github.com>
2025-02-22 16:35:24 -0600

    Update CreateListing.tsx
    Functionality completed, verified screen can create a listing. Will need to add validation later for forms and touch up UI

--- screens/CreateListing.tsx
+++ screens/CreateListing.tsx
@@ -1,110 +1,157 @@
-import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet } from 'react-native';
-import { createNewListing } from "../services/ListingsService"; // Will use this import for integrating front + back
+import React, { useState } from "react";
+import { View, Text, TextInput, Button, StyleSheet, Pressable, Alert, TouchableOpacity} from "react-native";
+
+import { createNewListing } from "../services/ListingsService"; // Ensure this is correctly imported
+import { Timestamp } from "firebase/firestore";
 
 export function CreateThisListing() {
-    const [listingAttribute, placeText] = useState({
-      productName: '',
-      category: '',
-      description: '',
-      imageUrl: '',
-      isNew: false,
-      price: '',
-      userID: '',
-      dateCreated:'',
-    });
+  const [productName, setProductName] = useState("");
+  const [category, setCategory] = useState("");
+  const [description, setDescription] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [isNew, setIsNew] = useState(false);
+  const [price, setPrice] = useState("");
+  const [userID, setUserID] = useState("");
 
-    return (
-        <View style={styles.basicLayout}>
+  return (
 
-        <TextInput
+    /* Form Fields */
+    <View style={styles.basicLayout}>
+      <TextInput
         style={styles.textField}
         placeholder="Product Name"
         placeholderTextColor="black"
-        value={listingAttribute.productName}
-        onChangeText={(text) => placeText({ ...listingAttribute, productName: text })}
-        />
+        value={productName}
+        onChangeText={setProductName}
+      />
 
-        <TextInput
+      <TextInput
         style={styles.textField}
         placeholder="Category"
         placeholderTextColor="black"
-        value={listingAttribute.productName}
-        onChangeText={(text) => placeText({ ...listingAttribute, category: text })}
-        />
+        value={category}
+        onChangeText={setCategory}
+      />
 
-        <TextInput
+      <TextInput
         style={styles.textField}
         placeholder="Description"
         placeholderTextColor="black"
-        value={listingAttribute.productName}
-        onChangeText={(text) => placeText({ ...listingAttribute, description: text })}
-        />
+        value={description}
+        onChangeText={setDescription}
+      />
 
-        <TextInput
+      <TextInput
         style={styles.textField}
-        placeholder="Image URL"
+        placeholder="Image Url"
         placeholderTextColor="black"
-        value={listingAttribute.productName}
-        onChangeText={(text) => placeText({ ...listingAttribute, imageUrl: text })}
-        />
+        value={imageUrl}
+        onChangeText={setImageUrl}
+      />
 
-
-        <TextInput
-        style={styles.textField}
-        placeholder="Is New"
-        placeholderTextColor="black"
-        value={listingAttribute.productName}
-        onChangeText={(text) => placeText({ ...listingAttribute, imageUrl: text })} // Fix this for is New atrribute 
-        />
-
-        <TextInput
+      <TextInput
         style={styles.textField}
         placeholder="Price"
         placeholderTextColor="black"
-        value={listingAttribute.productName}
-        onChangeText={(text) => placeText({ ...listingAttribute, price: text })}
-        />
+        keyboardType="numeric"
+        value={price}
+        onChangeText={setPrice}
+      />
 
-        <TextInput
+      <TextInput
         style={styles.textField}
         placeholder="User ID"
         placeholderTextColor="black"
-        value={listingAttribute.productName}
-        onChangeText={(text) => placeText({ ...listingAttribute, userID: text })}
-        />
+        value={userID}
+        onChangeText={setUserID}
+      />
 
-        <TextInput
-        style={styles.textField}
-        placeholder="Date Created"
-        placeholderTextColor="black"
-        value={listingAttribute.productName}
-        onChangeText={(text) => placeText({ ...listingAttribute, dateCreated: text })}
-        />
-     
-        </View>
-      );
-    }
+      {/* New/Used Buttons  */}
+      <View style={styles.buttonContainer}>
+        <Text style={styles.buttonText}>Condition:</Text>
+
+        <TouchableOpacity onPress={() => setIsNew(true)} style={styles.buttonLayout}>
+          <View style={[styles.buttonCircle, isNew && styles.optionPressed]} />
+          <Text>New</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => setIsNew(false)} style={styles.buttonLayout}>
+          <View style={[styles.buttonCircle, !isNew && styles.optionPressed]} />
+          <Text>Used</Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* Create Button - Need to add validation here  */}
+      <Button
+        title="Create This Listing"
+        onPress={() =>
+          // Call function from listing services - similar format for update and delete later as well 
+          createNewListing({
+            productName,
+            category,
+            description,
+            imageUrl,
+            isNew,
+            price,
+            userID,
+            dateCreated: Timestamp.now(),
+          })
+        }
+      />
+
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-    basicLayout: {
-      justifyContent: 'center', 
-      alignItems: 'center', 
-      flex: 1,
-      backgroundColor: 'white', 
-      padding: 10,
-    },
-    textField: {
-      borderColor: 'black', 
-      borderWidth: 5,
-      marginBottom: 15,
-      paddingHorizontal: 15,
-      color: 'black', 
-      backgroundColor: 'white', 
-      alignContent: 'center'
-    },
-    buttonText: {
-      color: 'black', 
-    }
-  });
-  
+  basicLayout: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+    backgroundColor: "white",
+    padding: 20,
+  },
+  textField: {
+    borderColor: "black",
+    borderWidth: 1,
+    marginBottom: 15,
+    paddingHorizontal: 15,
+    height: 40,
+    width: "90%",
+    borderRadius: 5,
+    backgroundColor: "white",
+    color: "black",
+    
+  },
+  buttonContainer: {
+    flexDirection: "row", 
+    alignItems: "center",
+    justifyContent: "flex-start", 
+    width: "90%",
+    marginBottom: 15,
+  },
+  buttonText: {
+    fontSize: 15,
+    marginRight: 10,
+  },
+  buttonLayout: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginHorizontal: 15,
+  },
+  buttonCircle: {
+    height: 20,
+    width: 20,
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+    marginRight: 8,
+  },
+  optionPressed: {
+    backgroundColor: "black", 
+  },
+ 
+});
+
