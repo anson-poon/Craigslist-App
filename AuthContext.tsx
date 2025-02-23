c72790eea971218e8152220ad362bf614dccf51b
alexcw08 <calex781@gmail.com>
2025-02-23 17:48:04 -0500

    Create auth context.

---
+++ AuthContext.tsx
@@ -0,0 +1,37 @@
+import { auth } from "./firebaseConfig";
+import { onAuthStateChanged, User } from "firebase/auth";
+import { createContext, useContext, useEffect, useState } from "react";
+
+interface AuthType {
+  user: User | null;
+  isLoading: boolean;
+}
+
+const AuthContext = createContext<AuthType | undefined>(undefined);
+
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [user, setUser] = useState<User | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const stopListener = onAuthStateChanged(auth, (firebaseUser) => {
+      setUser(firebaseUser);
+      setIsLoading(false);
+    });
+    return stopListener();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, isLoading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("This function must be used within the AuthProvider.");
+  }
+  return context;
+}
