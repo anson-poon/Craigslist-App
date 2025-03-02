f41fb226d681b6dba3594b72bb39e386623b38a5
Anson Poon <ansonpoon77@gmail.com>
2025-03-02 01:40:43 -0800

    Updated UploadImage component for dark mode

--- components/UploadImage.tsx
+++ components/UploadImage.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Text, View, TouchableOpacity, Image, StyleSheet, Dimensions } from "react-native";
 import * as ImagePicker from "expo-image-picker";
+import { useColorScheme } from "react-native";
 import { useIsFocused } from "@react-navigation/native";
 
 interface UploadImageProps {
@@ -8,6 +9,8 @@ interface UploadImageProps {
 }
 
 export default function UploadImage({ onImagePick }: UploadImageProps) {
+    const colorScheme = useColorScheme() ?? "light";
+    const styles = getStyles(colorScheme);
     const [image, setImage] = useState<string | null>(null);
     const isFocused = useIsFocused();
 
@@ -36,51 +39,55 @@ export default function UploadImage({ onImagePick }: UploadImageProps) {
     };
 
     return (
-        <View style={style.container}>
+        <View style={styles.container}>
             {image ? (
                 // If an image is selected, display the image
                 <TouchableOpacity onPress={pickImage}>
-                    <Image source={{ uri: image }} style={style.image} />
+                    <Image source={{ uri: image }} style={styles.image} />
                 </TouchableOpacity>
             ) : (
                 // If no image is selected, display the upload button
-                <TouchableOpacity onPress={pickImage} style={style.uploadButton}>
-                    <Text style={style.uploadText}>Select Image</Text>
+                <TouchableOpacity onPress={pickImage} style={styles.uploadButton}>
+                    <Text style={styles.uploadText}>Select Image</Text>
                 </TouchableOpacity>
             )}
         </View>
     );
 }
 
-const style = StyleSheet.create({
-    container: {
-        justifyContent: "center",
-        alignItems: "center",
-        width: Dimensions.get("window").width * 0.9,
-        height: Dimensions.get("window").width * 0.75,
-        marginTop: 20,
-        marginBottom: 20,
-        borderRadius: 10,
-        borderWidth: 1,
-        borderColor: "#ddd",
-        backgroundColor: "#444",
-    },
-    uploadButton: {
-        justifyContent: "center",
-        alignItems: "center",
-        padding: 8,
-        borderRadius: 10,
-        backgroundColor: "#f0f0f0",
-    },
-    uploadText: {
-        color: "#888",
-    },
-    image: {
-        width: Dimensions.get("window").width * 0.9,
-        height: Dimensions.get("window").width * 0.75,
-        resizeMode: "cover",
-        borderRadius: 10,
-        borderWidth: 2,
-        borderColor: "#ddd",
-    },
-});
+const getStyles = (colorScheme: "light" | "dark") => {
+    const backgroundColor = { light: "#999", dark: "#444" };
+
+    return StyleSheet.create({
+        container: {
+            justifyContent: "center",
+            alignItems: "center",
+            width: Dimensions.get("window").width * 0.9,
+            height: Dimensions.get("window").width * 0.75,
+            marginTop: 20,
+            marginBottom: 20,
+            borderRadius: 10,
+            borderWidth: 1,
+            borderColor: "#ddd",
+            backgroundColor: backgroundColor[colorScheme],
+        },
+        uploadButton: {
+            justifyContent: "center",
+            alignItems: "center",
+            padding: 8,
+            borderRadius: 10,
+            backgroundColor: "#f0f0f0",
+        },
+        uploadText: {
+            color: "#888",
+        },
+        image: {
+            width: Dimensions.get("window").width * 0.9,
+            height: Dimensions.get("window").width * 0.75,
+            resizeMode: "cover",
+            borderRadius: 10,
+            borderWidth: 2,
+            borderColor: "#ddd",
+        },
+    });
+};
